8da2732ba9c4c2267b1857ec68537909866591b5
louise-becker <louise.becker@gmail.com>
2022-01-13 14:46:09 +0100

    BE changed to heroku

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,3 +1,4 @@
-const BASE_URL = 'http://localhost:8080';
+// const BASE_URL = 'http://localhost:8080';
+const BASE_URL = 'https://thoughts-by-mamite-and-louise.herokuapp.com';
 
 export const API_URL = (slug) => `${BASE_URL}/${slug}`;
