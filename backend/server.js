13827efc9a50589179a8862ae9e2dba3ad63bd48
louise-becker <louise.becker@gmail.com>
2022-01-13 11:48:20 +0100

    basic frontend setup

--- backend/server.js
+++ backend/server.js
@@ -27,10 +27,7 @@ const UserSchema = new mongoose.Schema({
 
 const User = mongoose.model('User', UserSchema);
 
-// Defines the port the app will run on. Defaults to 8080, but can be
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
+// Defines the port the app will run on. Defaults to 8080
 const port = process.env.PORT || 8080;
 const app = express();
 
@@ -38,15 +35,45 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// checks if the access token was sent in the request, if not send message you are not logged in.
+// normally sent in the header in the request
+const authenticateUser = async (req, res, next) => {
+  const accessToken = req.header('Authorization');
+
+  try {
+    const user = await User.findOne({ accessToken });
+    if (user) {
+      next();
+    } else {
+      res.status(401).json({ response: 'Please, log in', success: false });
+    }
+  } catch (error) {
+    res.status(400).json({ response: error, success: false });
+  }
+};
+
+// Authentication - 401 (Unauthorized) But should be unauthenticated
+// Authorization - 403 (Forbidden) But should be unauthorized
+
 // Start defining your routes here
 app.get('/', (req, res) => {
   res.send('Hello world and Mamite & Louise');
+});
+
+//We use the ex of project Happy Thoughts
+
+app.get('/thoughts', authenticateUser);
+app.get('/thoughts', (req, res) => {
+  res.send('Here are your thoughts');
 });
 
 app.post('/signup', async (req, res) => {
   const { username, password } = req.body;
   try {
     const salt = bcrypt.genSaltSync();
+    if (password.length < 5) {
+      throw 'Password must be at least 5 characters long';
+    }
     const newUser = await new User({
       username,
       password: bcrypt.hashSync(password, salt),
@@ -69,6 +96,7 @@ app.post('/signin', async (req, res) => {
 
   try {
     const user = await User.findOne({ username });
+
     if (user && bcrypt.compareSync(password, user.password)) {
       res.status(200).json({
         response: {
@@ -79,7 +107,10 @@ app.post('/signin', async (req, res) => {
         success: true,
       });
     } else {
-      res.status(404).json({ response: 'User not found', success: false });
+      res.status(404).json({
+        response: "Username or password doesn't match",
+        success: false,
+      });
     }
   } catch (error) {
     res.status(400).json({ response: error, success: false });
